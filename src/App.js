d04904b2bd93a09dfda2ea537c87acd38ee2f352
Scrappy James <scrappjames2018@gmail.com>
2023-04-23 00:32:49 -0400

    First-commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Enter your Login info below</h1>
+      <textarea>Username</textarea>
+      <br></br>
+      <textarea>Password</textarea>
+      <br></br>
+      <button>Login</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
